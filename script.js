863b3ee2803b665d71a39ff438593bc27c83baa8
Guilherme Ambrósio <127554189+Guuiilhermee@users.noreply.github.com>
2025-11-30 19:41:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function principal(){
     let ppw = Number(document.getElementById('ppw').value)
     let m2dia = Number(document.getElementById('m2dia').value)
     let diasmes = Number(document.getElementById('diasmes').value)
-    let efisis = Number(document.getElementById('efisis').value) / 100
+    let efisis = Number(document.getElementById('efisis').value)
 
     let kwh = (ppw * m2dia * diasmes * efisis) / 1000
 
     console.log(`A geração mensal de energia por mês é: ${kwh.toFixed(2)}`)
 
     resposta.innerHTML = ``
-    resposta.innerHTML = `A geração mensal de energia é: ${kwh.toFixed(2)} KWH`
+    resposta.innerHTML = `A geração mensal de energia por mês é: ${kwh.toFixed(2)}`
 }
